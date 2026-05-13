e27319be70121410a9d4f3425be7c6b13d63c9de
R.M.P Harsha <prabhathharsha77@gmail.com>
2026-05-13 19:09:57 +0530

    remake app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,38 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home"
-import LogIn from "./pages/login"
-import SignUp from "./pages/signup";
-import AdminPage from "./pages/admin";
-import ForgotPassword from "./pages/forgotPassword";
+import { RouterProvider } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-
+import { router } from "./routes";
 
 function App() {
-  
-return (
-  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-       <BrowserRouter>
-    <div>
-     
-      <Toaster position="top-center"/>
-
-        <Routes path="/*">
-        
-          <Route path="/login" element={<LogIn/>}/>
-          <Route path="/signup" element={<SignUp/>} />
-          <Route path="/forgotPassword" element={<ForgotPassword/>} />
-          <Route path="/admin/*" element={<AdminPage/>} />
-          <Route path="/*" element={<Home />} />
-        </Routes> 
-        
-    </div>
-    
-   </BrowserRouter>
-    
-  </GoogleOAuthProvider>
-
-  )
+  return (
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <div>
+        <Toaster position="top-center" />
+        <RouterProvider router={router} />
+      </div>
+    </GoogleOAuthProvider>
+  );
 }
 
-export default App
+export default App;
