6336b3ee9ee58bd2370be2ce61fd8ee9503bc09a
R.M.P Harsha <prabhathharsha77@gmail.com>
2026-05-16 12:45:22 +0530

    remove review router

--- Backend/routes/productRouter.js
+++ Backend/routes/productRouter.js
@@ -1,5 +1,5 @@
 import express from "express"
-import {addReview, deleteProduct, getProduct, getProductById, saveProduct, searchProducts, updateProduct } from "../Controllers/productController.js"
+import { deleteProduct, getProduct, getProductById, saveProduct, searchProducts, updateProduct } from "../Controllers/productController.js"
 
 const productRouter = express.Router()
 
@@ -8,7 +8,6 @@ productRouter.get("/", getProduct);
 productRouter.delete("/:productId", deleteProduct);
 productRouter.put("/:productId",updateProduct);
 productRouter.get("/:productId", getProductById);
-productRouter.post("/:productId/reviews",addReview);
 productRouter.get("/search/:query", searchProducts);
 
 export default productRouter;
